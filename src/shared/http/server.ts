edfdb5a1701596879a20f8039a6cabfc642a21c2
Johnatan Dantas <dantas.johnatan@gmail.com>
2021-07-03 17:54:53 -0300

    create firts router

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,1 +1,15 @@
-console.log('Hello Dev!');
+import express from 'express';
+import cors from 'cors';
+
+import routes from '@shared/routes';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use(routes);
+
+app.listen(3333, () => {
+  console.log('Server started on port 3333! 🏆');
+});
