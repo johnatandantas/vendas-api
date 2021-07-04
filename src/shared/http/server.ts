fef951e38cce96963482afc0e2331a1965d3f773
Johnatan Dantas <dantas.johnatan@gmail.com>
2021-07-03 21:19:17 -0300

    add typeorm to the project

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,8 +1,10 @@
+import 'reflect-metadata';
 import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 
 import routes from '@shared/routes';
 import AppError from '@shared/errors/AppError';
+import '@shared/typeorm';
 
 const app = express();
 
